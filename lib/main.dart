188546c7960e0a2dc2c94cbc79698881e03e8088
Eunseong Han <gkstl119@gmail.com>
2024-09-25 23:12:29 -0400

    Implemented basicCreated the basic app structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+void main() {
+  runApp(WeatherInfoApp());
+}
+
+class WeatherInfoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather Info App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: WeatherHomePage(),
+    );
+  }
+}
